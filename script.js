f42bd01876212a71b0c04529a61cda3052c63d5d
mahmoud <mahmoudhammad3009@gmail.com>
2023-04-17 17:47:06 +0200

    make the page responsive

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-$(document).ready(function(){
-    $('.slider').slick({
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      centerMode: true,
-      variableWidth: true,
-      prevArrow: '<button type="button" class="custom-prev-arrow"><img src="./images/prev-arrow.png"/></button>',
-      nextArrow: '<button type="button" class="custom-next-arrow"><img src="./images/next-arrow.png"/></button>',
-      css: {
-        '.slick-prev': {
-          top: '0' // change the left position to your desired value
-        }
-      }
-    });
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    prevArrow:
+      '<button type="button" class="custom-prev-arrow"><img src="./images/prev-arrow.png"/></button>',
+    nextArrow:
+      '<button type="button" class="custom-next-arrow"><img src="./images/next-arrow.png"/></button>',
+    css: {
+      '.slick-prev': {
+        top: '0', // change the left position to your desired value
+      },
+    },
   });
+});
+
+const menu = document.querySelector('.menu');
+const navLinksWrapper = document.querySelector('.nav__links_wrapper');
+
+menu.addEventListener('click', () => {
+  if (navLinksWrapper.className.includes('menu_active'))
+    navLinksWrapper.classList.remove('menu_active');
+  else navLinksWrapper.classList.add('menu_active');
+});
